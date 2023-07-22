73b7312210477e6780685249161e2978bf3e1de7
ChewyChocolate <116084618+ChewyChocolate@users.noreply.github.com>
2023-07-22 10:56:56 +0800

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Main{
-	public static void main(String[] args) {
-		ArrayList<ArrayList<Integer>> index = new ArrayList<>();
-		int[] array = {1,2,3,4,5,6,7,8,9};
-		int n = 5;
-		
-		for(int i = 0; i < array.length; i++){
-		    for(int j = i+1; j < array.length; j++){
-		       if(i + j == n){
-		           ArrayList<Integer> pair = new ArrayList<>();
-		           pair.add(i);
-		           pair.add(j);
-		           index.add(pair);
-		       }
-		    }
-		}
-		
-		for(ArrayList pair : index){
-		    System.out.println(pair.get(0) + ", " + pair.get(1));
-		}
-		
-		
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> index = new ArrayList<>();
+
+        System.out.println("Enter numbers separated by spaces (Press Enter to end input):");
+        String inputLine = scan.nextLine();
+        System.out.println("What's the target number?");
+        int n = scan.nextInt();
+
+        // Split the input line into an array of strings
+        String[] numbersAsString = inputLine.split("\\s+");
+
+        // Convert the strings to integers and store them in an array
+        int[] array = new int[numbersAsString.length];
+        for (int i = 0; i < numbersAsString.length; i++) {
+            array[i] = Integer.parseInt(numbersAsString[i]);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == n) {
+                    ArrayList<Integer> pair = new ArrayList<>();
+                    pair.add(i);
+                    pair.add(j);
+                    index.add(pair);
+                }
+            }
+        }
+
+        for (ArrayList<Integer> pair : index) {
+            System.out.println(pair.get(0) + ", " + pair.get(1));
+        }
+    }
 }
